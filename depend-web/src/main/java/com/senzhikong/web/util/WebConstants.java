e995a6a1d0e5f3b6eee1e607fe4a25d385bb7e66
shu.zhou <997201652@qq.com>
2022-02-02 11:14:47 +0800

    合并用户认证

--- depend-web/src/main/java/com/senzhikong/web/util/WebConstants.java
+++ depend-web/src/main/java/com/senzhikong/web/util/WebConstants.java
@@ -5,13 +5,11 @@ public class WebConstants {
     public static final String USER_AUTH_CACHE= "user-auth";
     public static final String LOGIN_MODEL = "login-model-";
     public static final String LOGIN_USER_ID = "login-user-id";
-
-    public static final String MEMBER_AUTH_CACHE_NAME = "member-auth";
-    public static final String LOGIN_SYSTEM_ACCOUNT_INFO = "login-system-account-info";
-    public static final String LOGIN_MEMBER_ACCOUNT = "login-member-account";
     public static final String LOGIN_ACCOUNT_TOKEN = "login-account-token-";
 
+    public static final String LOGIN_SYSTEM_ACCOUNT_INFO = "login-system-account-info";
     public static final String SUPER_ADMINISTRATOR = "super";
+
     public final static String QRCODE_LOGIN_GRANT_THIRD_PART_USER = "qrcode-login-grant-third-part-user";
     public final static String QRCODE_LOGIN_GRANT_THIRD_PART_TOKEN = "qrcode-login-grant-third-part-token";
 
