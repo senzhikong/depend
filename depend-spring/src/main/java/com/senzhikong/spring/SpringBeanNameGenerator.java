a356d58165183beabd18ca9b6fde04b9614220e1
shu.zhou <997201652@qq.com>
2022-05-15 01:50:54 +0800

    20220515

--- depend-spring/src/main/java/com/senzhikong/spring/SpringBeanNameGenerator.java
+++ depend-spring/src/main/java/com/senzhikong/spring/SpringBeanNameGenerator.java
@@ -9,8 +9,8 @@ import org.springframework.beans.factory.support.BeanNameGenerator;
  * @date 2019年1月16日下午4:07:56
  */
 public class SpringBeanNameGenerator implements BeanNameGenerator {
- 
 
+    @Override
     public String generateBeanName(BeanDefinition definition, BeanDefinitionRegistry registry) {
         return definition.getBeanClassName();
     }
